4eeaab9c7f7926983e295bc5deaaa19d8c3a9c77
ezedquiroga <ezedaqui@gmail.com>
2024-05-30 22:22:06 -0300

    ProyectoFinal-JS

---
+++ app/data.js
@@ -0,0 +1,87 @@
+const myData = [
+  {
+    titulo: "Velas con esencias. 16 esencias disponibles",
+    imagen: "assets/img/imagen1.jpeg",
+    precio: "$7000",
+  },
+  {
+    titulo: "2",
+    imagen: "assets/img/imagen2.jpeg",
+    precio: "$7000",
+  },
+  {
+    titulo: "Kundalini Clear",
+    imagen: "assets/img/imagen3.jpeg",
+    precio: "$7000",
+  },
+  {
+    titulo: "4",
+    imagen: "assets/img/imagen4.jpeg",
+    precio: "$7000",
+  },
+  {
+    titulo: "5",
+    imagen: "assets/img/imagen5.jpeg",
+    precio: "$7000",
+  },
+  {
+    titulo: "6",
+    imagen: "assets/img/imagen6.jpeg",
+    precio: "$7000",
+  },
+  {
+    titulo: "7",
+    imagen: "assets/img/imagen7.jpeg",
+    precio: "$7000",
+  },
+  {
+    titulo: "8",
+    imagen: "assets/img/imagen8.jpeg",
+    precio: "$7000",
+  },
+  {
+    titulo: "9",
+    imagen: "assets/img/imagen9.jpeg",
+    precio: "$7000",
+  },
+  {
+    titulo: "10",
+    imagen: "assets/img/imagen10.jpeg",
+    precio: "$7000",
+  },
+  {
+    titulo: "11",
+    imagen: "assets/img/imagen11.jpeg",
+    precio: "$7000",
+  },
+  {
+    titulo: "12",
+    imagen: "assets/img/imagen12.jpeg",
+    precio: "$9000",
+  },
+  {
+    titulo: "13",
+    imagen: "assets/img/imagen13.jpeg",
+    precio: "$10500",
+  },
+  {
+    titulo: "14",
+    imagen: "assets/img/imagen14.jpeg",
+    precio: "$3500",
+  },
+  {
+    titulo: "15",
+    imagen: "assets/img/imagen15.jpeg",
+    precio: "$12000",
+  },
+  {
+    titulo: "16",
+    imagen: "assets/img/imagen16.jpeg",
+    precio: "$8000",
+  },
+  {
+    titulo: "17",
+    imagen: "assets/img/imagen17.jpeg",
+    precio: "$6000",
+  },
+];
